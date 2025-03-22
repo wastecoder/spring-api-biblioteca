7e61ce203cfc47298acbec6c3a2448fe4217df6e
Paulino <paulinomendes03@gmail.com>
2025-03-22 02:33:19 -0300

    feat(Railway): adiciona a dependência do driver do PostgreSQL e o perfil com variáveis de ambiente

--- src/main/java/spring/api/biblioteca/BibliotecaApplication.java
+++ src/main/java/spring/api/biblioteca/BibliotecaApplication.java
@@ -1,8 +1,14 @@
 package spring.api.biblioteca;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {
+		@Server(url = "/",
+				description = "Default Server URL")
+})
 @SpringBootApplication
 public class BibliotecaApplication {
 
